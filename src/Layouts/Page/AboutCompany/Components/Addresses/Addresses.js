bd7a022cbeee435ac604c6454ba66cbb87788474
pablochikoni <37252543+pablochikoni@users.noreply.github.com>
2023-01-30 19:21:36 +0000

    Addresses

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,17 +1,33 @@
-import PrimaryButton from "../../../../../Components/PrimaryButton";
+import styled from 'styled-components';
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+
 function Addresses() {
-    return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <SecondaryButton>Добавить филиал</SecondaryButton>
+    const Addresses = styled.div`
+    outline: none;
+    border-radius: 8px;
+    border: solid rgba(100,116,139,12%) 1px;
+    background-color: #ffffff;
+    color: #666787;
+    height: 420px;
+    width: 453px;
+`;
+
+const MText = styled.text`
+color: #0F172A;
+`;
+
+return (
+        <Addresses className='d-flex justify-content-between flex-column p-4 mt-5'>
+            <div className='d-flex flex-column gap-4'>
+                    <MText className='title'>Адреса заведений</MText>
+                    <div className='d-inline-flex flex-column gap-2'>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                    </div>
             </div>
-        </div>
-    );
+                    <SecondaryButton>Добавить филиал</SecondaryButton>
+            </Addresses>);
 }
 
 export default Addresses;
