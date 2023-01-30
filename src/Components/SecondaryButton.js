14abd77a190d8860dc85c64fdb633fb00dbfc68a
pablochikoni <37252543+pablochikoni@users.noreply.github.com>
2023-01-30 12:34:28 +0000

    Поправил секондари кнопку

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,18 +3,19 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
-        box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
+        background: #fff;
+        border: 1px solid rgba(100, 116, 139, 1);
+        border-radius: 24px;
         min-width: ${minWidth};
-        color: #152842;
+        color: #64748B;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: rgba(100, 116, 139, 0.8);
+            border: 0px;
         }
         &:active {
-            background: #DFDFDF;
+            background: #fff;
         }
     `;
     return (
