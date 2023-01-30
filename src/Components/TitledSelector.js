f6be013c529cd9bb66ebf373c16b9b01ebfe7b5a
pablochikoni <37252543+pablochikoni@users.noreply.github.com>
2023-01-30 18:55:44 +0000

    titledSelector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -7,23 +7,25 @@ const Select = styled.select`
     -webkit-appearance: none !important; 
     appearance: none !important;
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
+    color: #0F172A;
     &:hover {
-        background-color: #D9E2EFB8;
+        background-color: #F8FAFC;
     }
 `;
-
+const MText = styled.text`
+color: #94A3B8;
+`;
 function TitledSelector({title, options}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<MText className="caption">{title}</MText>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Select className='p-2'>
+            <Select className='p-2 text-rg'>
                 {options.map(optionName => {
                     return (
                         <option key={optionName}>{optionName}</option>
