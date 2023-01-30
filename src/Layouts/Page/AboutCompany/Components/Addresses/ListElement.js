0dc89f9304247e0c443466e407a218bb12d88b42
pablochikoni <37252543+pablochikoni@users.noreply.github.com>
2023-01-30 19:35:24 +0000

    List and Pref

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -4,21 +4,21 @@ import EditIcon from '../../../../../Icons/EditIcon';
 import RemoveIcon from '../../../../../Icons/RemoveIcon';
 import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
 
-const Option = styled.div`
+const Card = styled.div`
     outline: none;
     border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    border: solid #ffffff 1px;
+    background-color: #fff;
+    color: #64748B;
 `;
 
 function ListElement(props) {
     return (
-        <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
-            <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
-        </div>
+        <Card className='d-flex flex-row justify-content-between align-items-center gap-2 p-3 ps-0 pe-0'>
+        <text className='d-flex flex-grow-1'>{props.children}</text>
+        <IconOnlyButton icon={<EditIcon/>}></IconOnlyButton>
+        <IconOnlyButton icon={<RemoveIcon/>}></IconOnlyButton>
+    </Card>
     );
 }
 
--- src/Layouts/Page/AboutCompany/Components/General/Components/Preferences.js
+++ src/Layouts/Page/AboutCompany/Components/General/Components/Preferences.js
@@ -1,17 +1,25 @@
+import styled from "styled-components";
 import TitledTextInput from "../../../../../../Components/TitledTextInput";
 import TitledSelector from "../../../../../../Components/TitledSelector";
 import PrimaryButton from "../../../../../../Components/PrimaryButton";
+
+const Box = styled.div`
+width: 448px;
+`;
 function Preferences() {
     return (
-        <div className='d-inline-flex flex-column gap-4 mt-5'>
+        <Box className='d-inline-flex flex-column gap-4 ms-5 mt-0'>
+            <div className='d-flex flex-column gap-3'>;
             <TitledTextInput title='Название компании' def='SPBEAUTY'/>
             <TitledSelector title='Тип организации' options={[
                 'Салон красоты',
                 'Парикмахерская'
             ]}/>
             <TitledTextInput title='Основной телефон для связи' def='+7 (912) 345-67-89'/>
+
+            </div>
             <PrimaryButton>Сохранить</PrimaryButton>
-        </div>
+        </Box>
     );
 }
 
