cb38c09afff961aa2216371dee38b346bc620332
pablochikoni <37252543+pablochikoni@users.noreply.github.com>
2023-01-30 19:43:30 +0000

    Serach and melochi

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -1,45 +1,50 @@
-import styled from 'styled-components';
-import SearchIcon from '../../../../Icons/SearchIcon';
-import IconOnlyButton from '../../../../Components/IconOnlyButton';
+import styled from "styled-components";
+import SearchIcon from "../../../../Icons/SearchIcon";
+import IconOnlyButton from "../../../../Components/IconOnlyButton";
 
 const WrapperInput = styled.div`
-    position:relative;
-    width: 216px;
+   position: relative;
+  width: 216px;
 `;
 
 const Input = styled.input`
-    outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #152842;
-    width: 100%;
-    &:hover {
-        background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+outline: none;
+border-radius: 24px;
+border: solid #E2E8F0 1px;
+background-color: #F8FAFC;
+color: #94A3B8;
+width: 100%;
+&:hover {
+  background: rgba(248,250,252,80%);
+  border: 1px solid rgba(226,232,240,80%);
     }
-    &:focus {
-        background: white;
-    }
+&:focus {
+  background: F8FAFC;
+}
 `;
 
 const SearchIconWrapper = styled.div`
-    background-color: #4B759E;
-    position: absolute;
-    box-sizing:border-box;
-    right: 0px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
-    top:50%;
-    transform: translateY(-50%);
+background-color: rgba(255,255,255,0);
+position: absolute;
+box-sizing: border-box;
+right: 16px; /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
+top: 50%;
+transform: translateY(-50%);
 `;
 
 function SearchField() {
     return (
-        <WrapperInput className='d-inline-flex'>  {/* Устанавливает ширину поля и включает относительное положение */}
-            <Input className='p-2' placeholder='Найти на странице...'/>
-            <SearchIconWrapper className='p-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
-                <IconOnlyButton icon={<SearchIcon/>}/>
-            </SearchIconWrapper>
+        <WrapperInput className="d-inline-flex">
+          {" "}
+          {/* Устанавливает ширину поля и включает относительное положение */}
+          <Input className="p-2 text-rg ps-3" placeholder="Найти"/>
+          <SearchIconWrapper className="p-2">
+            {" "}
+            {/* Устанавливает абсолютное положение внутри поля ввода */}
+            <IconOnlyButton icon={<SearchIcon />} />
+          </SearchIconWrapper>
         </WrapperInput>
-    );
+      );
 }
 
 export default SearchField;
