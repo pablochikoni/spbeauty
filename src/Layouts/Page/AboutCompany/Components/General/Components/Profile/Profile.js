db20a5d1a71209d4b2b61d7fd16e6d53f1cc97ad
pablochikoni <37252543+pablochikoni@users.noreply.github.com>
2023-01-30 19:39:04 +0000

    profile

--- src/Layouts/Page/AboutCompany/Components/General/Components/Profile/Profile.js
+++ src/Layouts/Page/AboutCompany/Components/General/Components/Profile/Profile.js
@@ -1,12 +1,19 @@
+import styled from "styled-components";
 import Avatar from "./Avatar";
+
+const MText = styled.text`
+color: #64748B;
+`
+;
+
 function Profile() {
     return (
-        <div className='d-inline-flex flex-row'>
+        <div className='d-inline-flex flex-row mb-0 mt-5 ms-5'>
             <Avatar/>
             <div className='ms-3 d-inline-flex flex-column justify-content-center'>
                 <div className='title mb-2'>SPBEAUTY</div>
-                <div className="mb-1">Салон красоты</div>
-                <div>Санкт-Петербург, Невский пр-т. 28</div>
+                <MText className="mb-1">Салон красоты</MText>
+                <MText>Санкт-Петербург, Невский пр-т. 28</MText>
             </div>                  
         </div>
     );
